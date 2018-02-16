01e8e14e48ecd3cc78a4975a80fcb7a3e03cef58
afterloe <lm6289511@gmail.com>
2018-02-17 00:25:56 +0800

    feat: 补充下第二个简单服务的注释内容

--- day06/simpleService2/SimpleService.go
+++ day06/simpleService2/SimpleService.go
@@ -10,12 +10,14 @@ type CustomHandler struct {
 }
 
 func Execute(response http.ResponseWriter, request *http.Request) {
-	// 格式化输入参数 - 默认是不会格式化的
+	// 格式化输入参数 - 默认是不会格式化的 这个格式化只需要执行一次就可以了。
+	// 在使用前格式化，多次调用时没有效果的 eg: if r.Form == nil
 	request.ParseForm()
 	// 输出一些内容
 	fmt.Println(request.Form)
 	fmt.Println(request.Method) // 请求方式
 
+	// 自己写一个 函数Scheme 的校验规则
 	if 0 == len(request.Form["name"]) {
 		fmt.Fprintln(response, "lack parameter -> name")
 		return
@@ -26,12 +28,12 @@ func Execute(response http.ResponseWriter, request *http.Request) {
 }
 
 func (*CustomHandler) ServeHTTP(response http.ResponseWriter, request *http.Request) {
-	// 只处理 某个路径
+	// 只处理 某个路径 这里可用正则来扩充
 	if request.URL.Path == "/" {
 		Execute(response, request)
 		return
 	}
 
-	http.NotFound(response, request)
+	http.NotFound(response, request) // 自己也是可以编写 404 这类的返回的 源码只是 func NotFound(w ResponseWriter, r *Request)
 	return
 }
