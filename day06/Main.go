59b93f92809ed8d55b416a120a3497594c2edecc
afterloe <lm6289511@gmail.com>
2018-02-16 23:49:06 +0800

    feat: 封装部分函数包 -- 参考源码中的函数传入的方式并加入注解

--- day06/Main.go
+++ day06/Main.go
@@ -4,37 +4,25 @@ import (
 	"fmt"
 	"net/http"
 	"log"
-	"strings"
+	"./simpleService"
 )
 
-// 设置允许访问的域
-const POINT = "127.0.0.1:8082"
-
-// 路由函数
-func sayHello(response http.ResponseWriter, request *http.Request) {
-	// 格式化输入参数 - 默认是不会格式化的
-	request.ParseForm()
-	// 输出一些内容
-	fmt.Println(request.Form) // 返回是 map[string][]interface{}
-	fmt.Println(request.URL.Path)
-	fmt.Println(request.URL.Scheme)
-	// 这TM 返回的是一个 []interface{}
-	fmt.Println(request.Form["name"])
-
-	for key, val := range request.Form {
-		fmt.Printf("%s -> %s \n", key, strings.Join(val, " | "))
-	}
-	// 向输出流写入输出内容
-	fmt.Fprintf(response, "hello %s", request.Form["name"])
-}
-
 func main() {
+	serverListenAddr := simpleService.POINT
+
 	// 设置 访问路由
-	http.HandleFunc("/", sayHello)
+	/*
+	非 interface 的函数绑定，也许会更合适day05 中的 DBUtil 封装的方法
+
+		func HandleFunc(pattern string, handler func(ResponseWriter, *Request)) {
+			DefaultServeMux.HandleFunc(pattern, handler)
+		}
+	 */
+	http.HandleFunc("/", simpleService.Execute)
 	// 开始监听服务
-	err := http.ListenAndServe(POINT, nil)
+	err := http.ListenAndServe(serverListenAddr, nil)
 	if nil != err {
-		log.Fatal("Listen Port is", POINT, "Failed! because", err)
+		log.Fatal("Listen Port is", serverListenAddr, "Failed! because", err)
 	}
-	fmt.Println("service is success running ... in " + POINT)
+	fmt.Println("service is success running ... in " + serverListenAddr)
 }
