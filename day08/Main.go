484f14bfbb5953ac05d10d003085a5b76a879b32
afterloe <lm6289511@gmail.com>
2018-02-24 16:57:58 +0800

    feat: 添加配置项到main中

--- day08/Main.go
+++ day08/Main.go
@@ -5,7 +5,10 @@ import (
 	"os"
 	"fmt"
 	cli "./server"
+	"./integrate/logger"
+	"./config"
 	"time"
+	"strconv"
 )
 
 var (
@@ -20,13 +23,58 @@ func init() {
 	pid = os.Getpid()
 }
 
-func main() {
-	fmt.Printf("server init ... \n")
-	fmt.Printf("machine is %d cpus. \n", cpuNumber)
-	fmt.Printf("config thread number is null, will start %d thread to wrok default \n", cpuNumber)
-	runtime.GOMAXPROCS(cpuNumber)
-	fmt.Printf("listen parameter is null, will start server in %s default \n", defAddr)
-	fmt.Printf("server is init success... started pid is %d \n", pid)
-	fmt.Printf("[Cynomys] %v | started server success. \n", time.Now().Format("2006/01/02 - 15:04:05"))
+func startUpService(serverCfg map[string]interface{}) {
+	addr, port := serverCfg["addr"], serverCfg["port"]
+	if nil == addr {
+		addr = "127.0.0.1"
+	}
+	if nil == port {
+		port = "8080"
+	}
+	addrStr := fmt.Sprintf("%s:%s", addr, port)
+	logger.Info(fmt.Sprintf("will start server in %s ", addrStr))
+	cli.StartUpTCPServer(&addrStr)
+}
+
+func startUpMultiService(multiCfg map[string]interface{}) {
+	flg := multiCfg["enable"]
+	if nil == flg {
+		logger.Info("server not allow to use multi cpu")
+		return
+	}
+	if flg.(bool) {
+		coreNumber := multiCfg["num"]
+		logger.Info("server will to use multi cpu")
+		if nil == coreNumber {
+			coreNumber = cpuNumber
+		} else if 0 >= coreNumber.(int) {
+			coreNumber = cpuNumber
+		}
+		logger.Info(fmt.Sprintf("server will use %v cpu", coreNumber))
+		runtime.GOMAXPROCS(coreNumber.(int))
+	}
+}
+
+func startDefault() {
+	logger.Info(fmt.Sprintf("listen parameter is null, will start server in %s default", defAddr))
+	logger.Info(fmt.Sprintf("server is init success... started pid is %d", pid))
+	logger.Info("started server success.")
 	cli.StartUpTCPServer(&defAddr)
+}
+
+func main() {
+	logger.Info("server init ...")
+	logger.Info(fmt.Sprintf("machine is %d cpus.", cpuNumber))
+	serverCfg := config.Get("server").(map[string]interface{})
+	if nil == serverCfg {
+		startDefault()
+		return
+	}
+	multiCfg := serverCfg["multiCore"].(map[string]interface{})
+	if nil == multiCfg {
+		startUpService(serverCfg)
+	} else {
+		startUpMultiService(multiCfg)
+		startUpService(serverCfg)
+	}
 }
