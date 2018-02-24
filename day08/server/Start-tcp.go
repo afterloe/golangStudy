e2dad0230ce4d0dd5fbe05077de9779288b5ad77
afterloe <lm6289511@gmail.com>
2018-02-24 14:54:54 +0800

    feat: 自定义404页面

--- day08/server/Start-tcp.go
+++ day08/server/Start-tcp.go
@@ -3,21 +3,30 @@ package server
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
-	r "../routers"
+	"../routers"
 	"../integrate/logger"
+	"../integrate/notSupper"
 )
+
+var notFoundStr, notSupperStr string
+
+func init() {
+	notFoundStr = "route is not defined."
+	notSupperStr = "method is not supper"
+}
 
 func StartUpTCPServer(addr *string) {
 	gin.DisableConsoleColor()
 	gin.SetMode(gin.ReleaseMode)
-	routers := gin.New()
-	routers.Use(gin.Recovery())
-	routers.Use(logger.Logger())
-	v1 := routers.Group("/v1")
-	r.Execute(v1)
+	engine := gin.New()
+	engine.Use(gin.Recovery())
+	engine.Use(logger.Logger())
+	engine.NoRoute(notSupper.NotFound(&notFoundStr))
+	engine.NoMethod(notSupper.NotSupper(&notSupperStr))
+	routers.Execute(engine.Group("/v1"))
 	server := &http.Server{
 		Addr: *addr,
-		Handler: routers,
+		Handler: engine,
 		MaxHeaderBytes: 1 << 20,
 	}
 
