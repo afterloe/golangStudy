10e08e79ac4025be3386f8fc6f732f8a587cf597
afterloe <lm6289511@gmail.com>
2018-03-09 15:49:41 +0800

    feat: 镜像页面构建

--- day08/html/js/images.js
+++ day08/html/js/images.js
@@ -1,23 +1,111 @@
-ReactDOM.render(
-    <div>
-        <div className={"buttonList"}>
-            <button type="button" class="btn btn-primary">构建</button>
-            <button type="button" class="btn btn-secondary">上传</button>
-            <button type="button" class="btn btn-dark">管理</button>
-        </div>
-        <span class="cwWhete"></span>
-        <div>
-            <div class="card" style={{width: "200px"}}>
-                <img class="card-img-top" src="..." alt="Card image cap" />
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
+class ButtonManager extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className={"buttonList"}>
+                <button type="button" class="btn btn-primary">构建</button>
+                <button type="button" class="btn btn-secondary">上传</button>
+                <button type="button" class="btn btn-dark">管理</button>
             </div>
-        </div>
-        <span class="cwWhete"></span>
-    </div>,
+        )
+    }
+}
+
+class ImagesViews extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className={"card-deck"}>
+                <div class="card">
+                    <img class="card-img-top" src="../images/docker_image.jpg" alt="Card image cap" />
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">Last updated 3 mins ago</small>
+                    </div>
+                </div>
+                <div class="card">
+                    <img class="card-img-top" src="../images/docker_image.jpg" alt="Card image cap" />
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">Last updated 3 mins ago</small>
+                    </div>
+                </div>
+                <div class="card">
+                    <img class="card-img-top" src="../images/docker_image.jpg" alt="Card image cap" />
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">Last updated 3 mins ago</small>
+                    </div>
+                </div>
+                <div class="card">
+                    <img class="card-img-top" src="../images/docker_image.jpg" alt="Card image cap" />
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">Last updated 3 mins ago</small>
+                    </div>
+                </div>
+
+            </div>
+        )
+    }
+}
+
+class ImagesManager extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className={"imagesView"}>
+                <ButtonManager />
+                <span className={"cwWhete"}></span>
+                <ImagesViews />
+                <span className={"cwWhete"}></span>
+                <span className={"cwWhete"}></span>
+                <nav aria-label="Page navigation example">
+                    <ul class="pagination justify-content-end">
+                        <li class="page-item disabled">
+                            <a class="page-link" href="#" tabindex="-1">Previous</a>
+                        </li>
+                        <li class="page-item"><a class="page-link" href="#">1</a></li>
+                        <li class="page-item"><a class="page-link" href="#">2</a></li>
+                        <li class="page-item"><a class="page-link" href="#">3</a></li>
+                        <li class="page-item">
+                            <a class="page-link" href="#">Next</a>
+                        </li>
+                    </ul>
+                </nav>
+                <span class="cwWhete"></span>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <ImagesManager />,
     document.getElementById('root')
 );
 
