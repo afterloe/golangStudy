4cd45e84167ad1446c508b3fbd41edda2570c74f
afterloe <lm6289511@gmail.com>
2018-03-10 20:10:46 +0800

    feat: 构建创建镜像的页面

--- day08/html/js/manager/create.js
+++ day08/html/js/manager/create.js
@@ -1,6 +1,27 @@
 ReactDOM.render(
-    <div> hello world</div>
-, document.getElementById("root"));
+    <div className="createView">
+        <nav class="nav">
+            <a class="nav-link active" href="#"><span class="badge badge-danger">1</span> 上传tar包</a>
+            <a class="nav-link disabled" href="#"><span class="badge badge-danger">2</span> 构建</a>
+            <a class="nav-link disabled" href="#"><span class="badge badge-danger">3</span> 保存镜像</a>
+        </nav>
+        <span className={"cwWhete"}></span>
+        <div className={"uploadTar"}>
+            <form>
+                <div class="form-group">
+                    <label for="selectUploadFile" className={"form-control-lg"}>选择tar包</label>
+                    <input type="file" class="form-control-file form-control-lg"
+                           aria-describedby="fileHelp" placeholder="请上传镜像包." />
+                </div>
+                <small class="text-muted">
+                    请上传使用tar.gz 格式压缩的tar包，否则将无法解压。tar包中请包含 dockerfile
+                </small>
+                <span className={"cwWhete"}></span>
+                <button type="submit" class="btn btn-primary disabled float-right">下一步</button>
+            </form>
+        </div>
+    </div>
+    , document.getElementById("root"));
 
 const navbarData = {
     linkedHref: "/",
