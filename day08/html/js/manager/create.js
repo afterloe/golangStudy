f76369421471136847d0887b60a87911a47a370b
afterloe <lm6289511@gmail.com>
2018-03-10 23:09:00 +0800

    feat: 上传tar界面联动完成

--- day08/html/js/manager/create.js
+++ day08/html/js/manager/create.js
@@ -23,22 +23,86 @@ class ViewStep extends React.Component {
 class UploadTarApp extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            allowNextStep: false,
+        };
+        this.uploadFile = this.uploadFile.bind(this);
+        this.allowNext = this.allowNext.bind(this);
+        this.buildFileInstead = this.buildFileInstead.bind(this);
+    }
+
+    uploadFile(event) {
+        event.preventDefault();
+    }
+
+    allowNext(event) {
+        event.preventDefault();
+        const [file] = event.target.files;
+        console.log(file);
+        this.setState({allowNextStep: true, fileInstead: file, word: file.name});
+    }
+
+    formatTime(d = new Date()) {
+        return [ d.getFullYear(), d.getMonth() + 1, d.getDate() ].join('-')
+            + ' ' + [ d.getHours(), d.getMinutes(), d.getSeconds() ].join(':');
+    }
+
+    formatSize(d = 0) {
+        d = d / 1000;
+        return d > 1000 ? (d / 1000).toFixed(2) + " Mb": d.toFixed(3) + "Kb";
+    }
+
+    buildFileInstead() {
+        const {fileInstead} = this.state;
+        return (
+           <div className={"fileInstead"}>
+               <div class="form-group row">
+                   <label class="col-sm-3 col-form-label">文件名</label>
+                   <div class="col-sm-9">
+                       <input type="text" readonly class="form-control-plaintext" value={fileInstead.name}/>
+                   </div>
+               </div>
+               <div class="form-group row">
+                   <label class="col-sm-3 col-form-label">大小</label>
+                   <div class="col-sm-9">
+                       <input type="text" readonly class="form-control-plaintext"
+                              value={ this.formatSize(fileInstead.size) } />
+                   </div>
+               </div>
+               <div class="form-group row">
+                   <label class="col-sm-3 col-form-label">类型</label>
+                   <div class="col-sm-9">
+                       <input type="text" readonly class="form-control-plaintext" value={fileInstead.type} />
+                   </div>
+               </div>
+               <div class="form-group row">
+                   <label class="col-sm-3 col-form-label">上次修改时间</label>
+                   <div class="col-sm-9">
+                       <input type="text" readonly class="form-control-plaintext"
+                              value={this.formatTime(fileInstead.lastModifiedDate)} />
+                   </div>
+               </div>
+           </div>
+       )
     }
 
     render() {
+        const {allowNextStep = false, word = "请选择资源包"} = this.state;
         return (
             <div className={"uploadTar"}>
-                <form>
-                    <div class="form-group">
-                        <label for="selectUploadFile" className={"form-control-lg"}>选择tar包</label>
-                        <input type="file" class="form-control-file form-control-lg"
-                               aria-describedby="fileHelp" placeholder="请上传镜像包." />
+                <form onSubmit={this.uploadFile}>
+                    <div class="custom-file">
+                        <input type="file" class="custom-file-input" onChange={this.allowNext} />
+                        <label class="custom-file-label" for="customFile">{word}</label>
                     </div>
                     <small class="text-muted">
                         请上传使用tar.gz 格式压缩的tar包，否则将无法解压。tar包中请包含 dockerfile
                     </small>
+                    {allowNextStep? this.buildFileInstead(): ""}
                     <span className={"cwWhete"}></span>
-                    <button type="submit" class="btn btn-primary disabled float-right">下一步</button>
+                    <button type="submit" class= {
+                        allowNextStep ? "btn btn-primary float-right": "btn btn-primary float-right disabled"
+                    }>下一步</button>
                 </form>
             </div>
         )
