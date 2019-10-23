f8c53be230b855f5bb96399de1721886e7596073
afterloe <lm6289511@gmail.com>
2019-10-23 09:36:48 +0800

    feat: Interface

---
+++ src/officialGuide/10-tour.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+type I interface {
+	M()
+}
+
+type T struct {
+	S string
+}
+
+// 此方法表示类型 T 实现了接口 I，但我们无需显式声明此事。
+func (t T) M() {
+	fmt.Println(t.S)
+}
+
+func main() {
+	var i I = T{"hello"}
+	i.M()
+}
